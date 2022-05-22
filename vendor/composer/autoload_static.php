c28b707a8302097157d5dd0758fa1c754eb58c90
Kevin Ferreira <kevinferre210395@outlook.com>
2022-05-22 00:21:04 -0300

    autoload Hcode classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit9aa72cbfdfcdd750dc08a5558c8dcd61::$classMap;
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '0a266cc94bc392ea935bf31a2b9d969801486d02',
         'name' => 'kevinferre2103/ecommerce',
         'dev' => true,
     ),
     'versions' => array(
         'kevinferre2103/ecommerce' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '0a266cc94bc392ea935bf31a2b9d969801486d02',
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
